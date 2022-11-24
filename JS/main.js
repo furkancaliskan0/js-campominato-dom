d7887aa29dc800c2b454b23f9288779aa740fa0d
furkanGit <furkancaliskan9801@gmail.com>
2022-11-24 18:05:07 +0100

    Page Completed

--- JS/main.js
+++ JS/main.js
@@ -3,47 +3,88 @@
 //Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 //Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
-// creare contenitore 
-const contenitore = document.getElementById("container");
 
-console.log(contenitore);
+const playButton = document.getElementById('play-button');
+playButton.addEventListener('click' , 
+function(){
 
+    let difficultyOption = document.getElementById('difficulty');
+    console.log(difficultyOption.value);
 
-// creare bottone
-const button = document.getElementById("button");
+    const gridElement = document.getElementById('container');
+    gridElement.innerHTML ='';
+    let boxNumber = 0;
 
-button.addEventListener ("click",
-
-    function() {
-        contenitore.innerHTML = '';
-        console.clear();
-        for (let i = 1; i <= 100; i++ ){
-            let myElement = createSquare(i);
-            myElement.addEventListener("click",
-                function() {
-                    this.classList.add("clicked");
-                    console.log('Hai cliccato la cella:', i);
-                }
-            )
-            contenitore.appendChild(myElement);
-        }
+    if(difficultyOption.value == 'facile'){
+        boxNumber = 100;
+        console.log(boxNumber);
+    }else if (difficultyOption.value == 'medio'){
+        boxNumber = 81;
+        console.log(boxNumber);
+    }else{
+        boxNumber = 49;
+        console.log(boxNumber);
     }
-)
 
-function createSquare (i){
+    const bombsNumber = bombs(16, boxNumber);
+    console.log(bombsNumber);
 
-    const element = document.createElement("div");
-    element.classList.add("box");
-    element.innerHTML = i;
+    let points = 0;
+    
+    for(let i = 0; i < boxNumber; i++){
+        const gridBox = document.createElement('div');
+        
+        gridBox.addEventListener('click' , 
+        function(){
 
-    return element;
+            if(bombsNumber.includes(i)){
+                gridBox.classList.add('checked-bombs')
+                writeInElementById('score', `Hai perso il tuo punteggio è: ${points + 1}`);
+            }else{
+                writeInElementById('score', `il tuo punteggio è: ${points + 1}`);
+                points++;
+                gridBox.classList.add('checked');
+            }
+            
+        })
+            if(difficultyOption.value == 'facile'){
+                gridBox.classList.add('box-facile');
+                gridBox.innerHTML += i + 1;
+            }else if (difficultyOption.value == 'medio'){
+                gridBox.classList.add('box-medio');
+                gridBox.innerHTML += i + 1;
+            }else{
+                gridBox.classList.add('box-difficile');
+                gridBox.innerHTML += i + 1;
+            }
+            
+            gridElement.appendChild(gridBox);
+    }
+    
+});
 
-}
 
+    function bombs (numberBombs, Cells){
+        const arrayBombs = [];
+        for(let i = 0; i < numberBombs; i++){
+            arrayBombs.push(generateUniqueRandomNumber(arrayBombs, 1, Cells ));
+        }
+        return arrayBombs;
+    }
 
-function numRandom (min, max){
-    return (Math.floor(Math.random() * ((max + 1) - min) + min));
-}
+    function generateUniqueRandomNumber (numbArray, min ,max){
+        let check = false;
+        let random;
 
+        while(!check){
+            random = Math.floor(Math.random() * ((max + 1 ) - min) + min);
+            if( !numbArray.includes(random)){
+                check = true;
+            }
+        }
+        return random;
+    }
 
-
+    function writeInElementById(elementId, string){
+        document.getElementById(elementId).innerHTML = string;
+    }
